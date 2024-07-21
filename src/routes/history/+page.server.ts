e1982d4d71839faecd97ef85138c10f595dfe918
DaoNhutThi <114686399+DaoNhutThi@users.noreply.github.com>
2024-07-22 00:08:20 +0700

    update page service (delete token)

--- src/routes/history/+page.server.ts
+++ src/routes/history/+page.server.ts
@@ -2,7 +2,6 @@ import http from '$utils/http';
 import type { PageServerLoad } from './$types';
 
 export const load = (async () => {
-    http.defaults.headers.authorization= 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI3MjY1NjY5MjkyNzY1NTkzNiIsImlhdCI6MTcyMTM4OTkwNSwic3ViIjoiNjg2MzQxOTM0NjIwNjMxMDQiLCJuYW1lIjoiYWRtaW5pc3RyYXRvciIsImVtYWlsIjoiYWRtaW5pc3RyYXRvckB5b21pa2F6ZS5vcmciLCJzaWQiOiJFVDVQRlQ1T0hRRTVYUFQ1NExYREZRVFpPSlVKTUtONiIsInJvbGVzIjpbIlN1cGVyIiwiQWRtaW5pc3RyYXRvciJdfQ.f5G67sGwGy1SpfyW5RB1MxNYxkKEyRTW8bCoKPRX00c'
     return {
         historyEntries: http.get('/history')
     };
