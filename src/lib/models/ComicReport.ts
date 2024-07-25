05e80e1ade4437c45e999995fd628f8fd5b9714a
DaiPhuc <phucvddce160106@fpt.edu.vn>
2024-07-26 04:58:55 +0700

    Report Comic

--- src/lib/models/ComicReport.ts
+++ src/lib/models/ComicReport.ts
@@ -1,11 +1,13 @@
-import type Comic from "./Comic";
-import type Model from "./Model";
+import type Profile from '$components/profile.svelte';
+import type Comic from './Comic';
+import type Model from './Model';
 
-export default interface ComicReport extends Model{
-    description: string;
-    images: string;
-    categoryId: string;
-    reporterId: string;
-    readonly comic: Comic;
-    
+export default interface ComicReport extends Model {
+  description: string;
+  categoryId: string;
+  reporterId: string;
+  dismissalReason: string;
+  reporter: Profile;
+  readonly status: string;
+  readonly comic: Comic;
 }
