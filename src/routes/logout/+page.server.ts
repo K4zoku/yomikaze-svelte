2c9aeb83523027662526ef0b3c066640b5a6d651
Kazoku <k4zoku@pm.me>
2024-07-26 19:39:04 +0700

    Update server logic

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,9 +1,10 @@
+import { invalidateAll } from "$app/navigation";
 import { logout } from "$utils/auth-utils";
+import { redirect } from "@sveltejs/kit";
 import type { PageServerLoad } from "./$types";
 
 export const load : PageServerLoad = (async ({ cookies }) => {
     logout(cookies);
-    return {
-
-    };
+    invalidateAll();
+    throw redirect(302, '/');
 }) satisfies PageServerLoad;
