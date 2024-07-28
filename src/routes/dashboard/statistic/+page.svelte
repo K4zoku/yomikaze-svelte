88157684c1081d2c47dec94110cc9b6265c77961
khoatdt <88079474+khoatdt@users.noreply.github.com>
2024-07-29 02:30:02 +0700

    commit

---
+++ src/routes/dashboard/statistic/+page.svelte
@@ -0,0 +1,192 @@
+<script>
+
+</script>
+
+<div class="container mx-auto mt-10">
+	<div class="pt-20">
+		<span class="text-xl font-bold">General Statistics</span>
+	</div>
+	<div class="grid grid-cols-4 gap-4 mt-3">
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Comic</span>
+					<span class="text-xl font-semibold">100</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><path
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M2 3h6a4 4 0 0 1 4 4v14a3 3 0 0 0-3-3H2zm20 0h-6a4 4 0 0 0-4 4v14a3 3 0 0 1 3-3h7z"
+						/></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>User</span>
+					<span class="text-xl font-semibold">35</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path d="M18 21a8 8 0 0 0-16 0" /><circle cx="10" cy="8" r="5" /><path
+								d="M22 20c0-3.37-2-6.5-4-8a5 5 0 0 0-.45-8.3"
+							/></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Comment</span>
+					<span class="text-xl font-semibold">100</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><path
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							d="M14 9a2 2 0 0 1-2 2H6l-4 4V4c0-1.1.9-2 2-2h8a2 2 0 0 1 2 2zm4 0h2a2 2 0 0 1 2 2v11l-4-4h-6a2 2 0 0 1-2-2v-1"
+						/></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Income</span>
+					<span class="text-xl font-semibold">$66,000</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path
+								d="M4 2v20l2-1l2 1l2-1l2 1l2-1l2 1l2-1l2 1V2l-2 1l-2-1l-2 1l-2-1l-2 1l-2-1l-2 1Z"
+							/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8m4 1.5v-11" /></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Tags</span>
+					<span class="text-xl font-semibold">111</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path d="m15 5l6.3 6.3a2.4 2.4 0 0 1 0 3.4L17 19" /><path
+								d="M9.586 5.586A2 2 0 0 0 8.172 5H3a1 1 0 0 0-1 1v5.172a2 2 0 0 0 .586 1.414L8.29 18.29a2.426 2.426 0 0 0 3.42 0l3.58-3.58a2.426 2.426 0 0 0 0-3.42z"
+							/><circle cx="6.5" cy="9.5" r=".5" fill="currentColor" /></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Role Request</span>
+					<span class="text-xl font-semibold">500</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path
+								d="m18 5l-2.414-2.414A2 2 0 0 0 14.172 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2"
+							/><path
+								d="M21.378 12.626a1 1 0 0 0-3.004-3.004l-4.01 4.012a2 2 0 0 0-.506.854l-.837 2.87a.5.5 0 0 0 .62.62l2.87-.837a2 2 0 0 0 .854-.506zM8 18h1"
+							/></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Report</span>
+					<span class="text-xl font-semibold">$66,000</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path d="M15 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V7Z" /><path
+								d="M14 2v4a2 2 0 0 0 2 2h4m-5.5 4.5l-5 5m0-5l5 5"
+							/></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+
+		<div class="rounded-md border-2 p-2 w-60 border-accent-content shadow-lg">
+			<div class="flex justify-between">
+				<div class="flex flex-col">
+					<span>Outcome</span>
+					<span class="text-xl font-semibold">-$21,000</span>
+				</div>
+				<div class="rounded my-auto p-1 bg-warning">
+					<svg xmlns="http://www.w3.org/2000/svg" width="2em" height="2em" viewBox="0 0 24 24"
+						><g
+							fill="none"
+							stroke="currentColor"
+							stroke-linecap="round"
+							stroke-linejoin="round"
+							stroke-width="2"
+							><path
+								d="M4 2v20l2-1l2 1l2-1l2 1l2-1l2 1l2-1l2 1V2l-2 1l-2-1l-2 1l-2-1l-2 1l-2-1l-2 1Z"
+							/><path d="M16 8h-6a2 2 0 1 0 0 4h4a2 2 0 1 1 0 4H8m4 1.5v-11" /></g
+						></svg
+					>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
