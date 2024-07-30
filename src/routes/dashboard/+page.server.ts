ae4d98a8a9b14d14dad6bff595aba46cc11f8627
Kazoku <k4zoku@pm.me>
2024-07-30 19:09:23 +0700

    Added chart

--- src/routes/dashboard/+page.server.ts
+++ src/routes/dashboard/+page.server.ts
@@ -1,13 +1,15 @@
-import { getToken, hasRoles } from "$utils/auth-server-utils";
+import { getAndVerifyToken, hasRoles } from "$utils/auth-server-utils";
 import { error } from "@sveltejs/kit";
-import type { PageServerLoad } from "../$types";
+import type { PageServerLoad } from "./$types";
 
 const ALLOWED_ROLES = ['Super', 'Administrator', 'Publisher'];
 
 export const load : PageServerLoad = (async ({ cookies }) => {
-   const token: string = await getToken(cookies);
+   const token: string = await getAndVerifyToken(cookies);
    if (!await hasRoles(token, ALLOWED_ROLES)) {
       throw error(403, 'You do not have permission to access this page');
    }
-   return {};
+   return {
+      token,
+   };
 }) satisfies PageServerLoad;
