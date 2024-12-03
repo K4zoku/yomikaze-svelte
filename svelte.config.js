983452a7f61f7eea69f9e70052e4c5386074a0b8
Kazoku <k4zoku@pm.me>
2024-12-03 20:19:39 +0700

    feat: cloudflare pages

--- svelte.config.js
+++ svelte.config.js
@@ -1,4 +1,4 @@
-import adapter from 'svelte-adapter-bun';
+import adapter from '@sveltejs/adapter-cloudflare';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import path from 'path';
 import { readFileSync } from 'fs';
@@ -10,27 +10,21 @@ const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-  // Consult https://kit.svelte.dev/docs/integrations#preprocessors
-  // for more information about preprocessors
   preprocess: vitePreprocess(),
 
   kit: {
-    // adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-    // If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-    // See https://kit.svelte.dev/docs/adapters for more information about adapters.
     adapter: adapter({
-      out: 'build',
-      assets: true,
-      development: false,
-      // precompress: true,
-      precompress: {
-        brotli: true,
-        gzip: true,
-        files: ['htm', 'html']
-      },
-      dynamic_origin: true,
-      xff_depth: 1
-    }),
+			routes: {
+				include: ['/*'],
+				exclude: ['<all>']
+			},
+			platformProxy: {
+				configPath: 'wrangler.toml',
+				environment: undefined,
+				experimentalJsonConfig: false,
+				persist: false
+			}
+		}),
     alias: {
       $components: path.resolve('./src/lib/components'),
       $models: path.resolve('./src/lib/models'),
